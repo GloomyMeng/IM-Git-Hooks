f3ffe0b00fbd63640aa1de05d56e9e13245082ab
gloomy <mengyufeng@didiglobal.com>
2020-05-20 18:37:18 +0800

    Updated: ignore merge

--- commit-msg.sh
+++ commit-msg.sh
@@ -20,6 +20,13 @@ crashLogMsg() {
 	echo "     crashed:    会引起 crash 的 bug"
 }
 
+# 忽略merge request
+MERGE_MSG=`cat $1 | egrep '^Merge branch*'`
+
+if [ "$MERGE_MSG" != "" ]; then
+    exit 0
+fi
+
 COMMIT_MSG=`cat $1 | egrep "^(feat|fix|docs|refactor|build|chore|test)\[\w+\]?:\s(\S|\w)+"`
 
 if [ "$COMMIT_MSG" = "" ]; then
--- simple-commit-msg.sh
+++ simple-commit-msg.sh
@@ -8,7 +8,16 @@ crashLogMsg() {
 	echo "     crashed:    会引起 crash 的 bug"
 }
 
+# 忽略merge request
+MERGE_MSG=`cat $1 | egrep '^Merge branch*'`
+
+if [ "$MERGE_MSG" != "" ]; then
+    exit 0
+fi
+
+
 COMMIT_MSG=`cat $1 | egrep "^(feat|fix|docs|refactor|build|chore|test)\[\w+\]?:\s(\S|\w)+"`
+
 RESULT=`cat $1 | egrep "^fix\[(crashed|normal)]?:\sjira\s[0-9]+"`
 if [[ "$COMMIT_MSG" =~ ^fix.* ]]; then
 	RESULT=`cat $1 | egrep "^fix\[(crashed|normal)]?:\sjira\s[0-9]+"`
